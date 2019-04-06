c35081e955a55d505cc2771e361e0d87960ef234
Catrina Mihela <catrina.mihaela20@gmail.com>
2019-04-07 00:41:41 +0300

    Update send_email

---
+++ backend/app/controllers/badge.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, request, jsonify
+
+from app.models.badges import Badges
+from app.models.events import Events
+from app.models.users_badges import Users_Badges
+
+blueprint = Blueprint('badge', __name__, url_prefix='/badge')
+
+
+def create(user_id, badge_id):
+    Users_Badges.create(user=user_id, badge=badge_id)
+    return jsonify({'success': True, 'message': 'Your badge was created'}), 200
+
+
+def create_specific_badge(type, user_id):
+    badge = Badges.get(Badges.id == type)
+    create(user_id, badge.id)
+
+
+@blueprint.route('/invite', methods=['POST'])
+def invite():
+
+
+    return jsonify({'success': True, 'message': 'Your event was created'}), 200
+
+
+@blueprint.route('/all', methods=['GET'])
+def all():
+    allEvents = []
+    for event in Events.select():
+        dict = {'title': event.title, 'description': event.description, 'start': event.time_start, 'end': event.time_end}
+        allEvents.append(dict)
+
+    return jsonify({'success': True, 'message': allEvents}), 200
+
